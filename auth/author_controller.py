460488f8c2cd8efb154271b181f3891fc508a136
BrendahBanks <ampurirebrendah7@gmail.com>
2025-03-05 17:27:08 +0300

    group-work

--- auth/author_controller.py
+++ auth/author_controller.py
@@ -1,12 +1,52 @@
-# auth/author_controller.py
 
-from flask import Blueprint
 
-# Define the blueprint
-author_bp = Blueprint('author', __name__)
+from flask import Blueprint, request, jsonify
+from app import db
+from models import Author
 
-# Example route
-@author_bp.route('/')
-def index():
-    return "Author page"
+author_bp= Blueprint("author",__name__)
 
+# Create Author
+@author_bp.route('/',methods=['POST'])
+def create_author():
+    data = request.get_json()
+    new_author=Author(name=data["name"],biography=data.get("biograpy"))
+    db.session.add(new_author)
+    db.session.commit()
+    return jsonify({'message': ' Author created'}),200
+
+# Get all authors
+@author_bp.route('/',methods=['GET'])
+def get_all_author():
+    authors= Author.query.all()
+    return jsonify([{'id': author.id, 'name':author.name, 'biography':author.biography}for author in authors])
+
+# Get Author by Id
+@author_bp.route('/<int:id>', methods=['GET'])
+def get_all_authors():
+    author = Author.query.all(id)
+    if author:
+        return jsonify({'id': author.id, 'name':author.name,'biography':author.biography})
+    return jsonify({'message': ' Author not found'}) , 200
+
+# Update Author
+@author_bp.route('/<int:id>', methods=['put'])
+def update_author(id):
+    author=Author.query.get(id)
+    if author:
+        data = request.get_json()
+        author.name=data.get('name')
+        author.biography =data.get('biography')
+        db.session.commit()
+        return jsonify({'message': " Author updated"})
+    return jsonify({'message': ' Author not found'}),205
+
+# Delete Author
+@author_bp.route('/<int:id>', methods=['DELETE'])
+def delete_author(id):
+    author = Author.query.get(id)
+    if author:
+        db.session.delete(author)
+        db.session.commit()
+        return jsonify({'message': ' Author deleted'})
+    return jsonify({'message': ' Author not found'}) ,700
