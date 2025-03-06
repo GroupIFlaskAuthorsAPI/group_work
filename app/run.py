cfbd1da322b1ec65194553227970abc839ad28b2
nasejje mariam lumala <aziirwemercy@gmail.com>
2025-03-06 08:29:12 +0300

    group work

--- app/run.py
+++ app/run.py
@@ -1,6 +1,11 @@
-from app import create_app
+from app import create_app, db
+from app.models import Author, Book, Company  # Import your models to ensure tables are created
 
 app = create_app()
 
+# This ensures that tables are created before running the app
+with app.app_context():
+    db.create_all()  # This will create the tables if they don't already exist
+
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
